5f189bdf0fbbccb67892275470539f9deb7fa76c
Jack Harley <jackpharley@gmail.com>
2012-05-21 21:56:09 +0100

    Bye bye old ajax theme, we're done converting from it :)

--- themes/outerempires/index.tpl.php
+++ themes/outerempires/index.tpl.php
@@ -4,3 +4,25 @@
 	Home
 {% endblock %}
 
+{% block content %}
+	<h2>Welcome to the SWAT/FIRE Faction Site</h2>
+
+	<p>
+		We provide several utilities for non members, including player stats, faction stats and system stats. If you are a member, please see the post regarding this site in the ingame faction forums to get access to the members only utilities.<br />
+		Please feel free to explore the site!
+	</p>
+
+	<p>
+		--The SWAT Leadership
+	</p>
+
+	<p>
+		--The FIRE Leadership
+	</p>
+
+	<p>
+		--Jedi Jackian<br />
+		Admin
+	</p>
+{% endblock %}
+
