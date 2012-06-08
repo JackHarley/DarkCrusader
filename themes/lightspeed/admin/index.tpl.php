cbbb79e1d494a136d2635be89f9ca3df728df44b
Jack Harley <jackpharley@gmail.com>
2012-06-08 18:10:00 +0100

    1.0.007-3

--- themes/lightspeed/admin/index.tpl.php
+++ themes/lightspeed/admin/index.tpl.php
@@ -7,8 +7,7 @@
 <div class="box grid_16 tabs">
 					<div class="side_holder">
 						<ul class="tab_sider clearfix">
-							<li><a href="#tabs-a">Site Status</a></li>		
-							<li><a href="#tabs-e">User Statistics</a></li>	
+							<li><a href="#tabs-a">Site Status</a></li>
 						</ul>
 					</div>
 					<div id="tabs-a" class="block tab_sider">
@@ -17,22 +16,4 @@
 							<li><span style="color:green">ONLINE</span>Desktop Site</li>
 						</ul>
 					</div>
-					<div id="tabs-e" class="block tab_sider">
-						<h2>User Statistics</h2>
-						
-						<ul class="full_width">
-							<li><span>{{userStats.userCount}}</span>Users</li>
-							<li><span>{{userStats.userGroupCount}}</span>User Groups</li>
-						</ul>
-						<br />
-						
-						<h2>Groups</h2>
-						
-						<ul class="full_width">
-							{% for group in groups %}
-								<li><span>{{group.userCount}}</span>{{group.description}}</li>
-							{% endfor %}
-						</ul>
-					</div>
-				</div>	
 {% endblock %}
