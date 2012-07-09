e6e5a187c44564acbbd0c38ec9708877070e638f
Jack Harley <jackpharley@gmail.com>
2012-07-09 19:38:33 +0100

    Google Analytics for lightspeed theme

--- themes/lightspeed/base.tpl.php
+++ themes/lightspeed/base.tpl.php
@@ -5,6 +5,23 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>SWAT/FIRE -> {% block pageTitle %}{% endblock %}</title>
 	<link rel="stylesheet" type="text/css" href="{% viewurl /style.css %}" media="screen" />
+
+	<!-- google analytics -->
+	{% if ! exists userIsAdmin %}
+		{% if exists googleAnalyticsCode %}
+			<script type="text/javascript">
+				var _gaq = _gaq || [];
+				_gaq.push(['_setAccount', '{{googleAnalyticsCode}}']);
+				_gaq.push(['_trackPageview']);
+
+				(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+				})();
+			</script>
+		{% endif %}
+	{% endif %}
 </head>
 
 <body>
