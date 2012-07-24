1992e6ac117622f7057c6aa578b4eaa534050bb7
Jack Harley <jackpharley@gmail.com>
2012-07-24 14:21:31 +0200

    Added option to pop out chat

--- themes/lightspeed/chat.tpl.php
+++ themes/lightspeed/chat.tpl.php
@@ -7,8 +7,9 @@
 
 <p>
 	Simply click the Connect button below to connect to our secure IRC channel.<br />
+	<a target="_blank" href="http://qchat.rizon.net/?nick={{nickname}}&channels={{channel}}{% if exists key %}%20{{key}}{% endif %}&uio=d4">Click here to open a new resizeable window with the chat in it.</a><br />
 	Alternatively, use an IRC client to connect to irc.rizon.net #{{channel}} {% if exists key %}with the channel key {{key}}{% endif %}
 </p>
 
-<iframe src="http://qchat.rizon.net/?nick={{nickname}}&channels={{channel}}{% if exists key %}%20{{key}}{% endif %}&uio=d4" width="100%" height="370"></iframe>
+<iframe src="http://qchat.rizon.net/?nick={{nickname}}&channels={{channel}}{% if exists key %}%20{{key}}{% endif %}&uio=d4" width="100%" height="350"></iframe>
 {% endblock %}
