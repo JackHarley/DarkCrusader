b6e8f9750ec4f728486ae791f3b3f3322d460671
AwesomezGuy <jackpharley@gmail.com>
2012-08-14 14:12:47 +0100

    Hotfix

--- themes/jjutilities/empire/index.tpl.php
+++ themes/jjutilities/empire/index.tpl.php
@@ -5,9 +5,21 @@
 {% block content %}
 <h2>Empire Management</h2>
 
+<h3>Finances</h3>
+<ul>
+	<li>Worker Costs in Past Week: {{workerCostsLastWeek|numberformat}}c</li>
+	<li>Market Sales in Past Week: {{marketSalesLastWeek|numberformat}}c</li>
+	{% if profitOrLossLastWeek > 0 %}
+		<li>Profit in Past Week: <span style="color:lime">{{profitOrLossLastWeek|numberformat}}c</span></li>
+	{% else %}
+		<li>Loss in Past Week: <span style="color:red">{{profitOrLossLastWeek|numberformat}}c</span></li>
+	{% endif %}
+</ul>
+
 <p>
 	<a href="{% url /index.php/empire/seller %}">See an overview of your market sales</a><br />
-	<a href="{% url /index.php/empire/resources %}">See an overview of the resources you have stored around the galaxy</a>
+	<a href="{% url /index.php/empire/resources %}">See an overview of the resources you have stored around the galaxy</a><br />
+	<a href="{% url /index.php/empire/colonies %}">See an overview of your colonies</a>
 </p>
 
 {% endblock %}
