d7e48525a06b8774e0c3609f8e2f6d87f364048b
Jack Harley <jackpharley@gmail.com>
2012-09-14 20:07:26 +0100

    JJ

--- lib/darkcrusader/controllers/GameportalController.php
+++ lib/darkcrusader/controllers/GameportalController.php
@@ -23,7 +23,22 @@ class GameportalController extends Controller {
 		$user = UserModel::getInstance()->getActiveUser();
 		View::setVar("nickname", str_replace(" ", "", $user->username));
 
-		View::load('game_portal', array(
+		View::load('game_portal/index', array(
+			"channelString" => Config::getRequiredVal("chat", "private_channel") . "%20" . Config::getRequiredVal("chat", "private_channel_key"),
+			"connectString" => "irc.rizon.net and join #" . Config::getRequiredVal("chat", "private_channel") . " with the key " . Config::getRequiredVal("chat", "private_channel_key")
+		));
+	}
+
+	public function dual() {
+		$this->checkAuth(array(
+			"access_chat",
+			"access_private_chat"
+		));
+
+		$user = UserModel::getInstance()->getActiveUser();
+		View::setVar("nickname", str_replace(" ", "", $user->username));
+
+		View::load('game_portal/dual', array(
 			"channelString" => Config::getRequiredVal("chat", "private_channel") . "%20" . Config::getRequiredVal("chat", "private_channel_key"),
 			"connectString" => "irc.rizon.net and join #" . Config::getRequiredVal("chat", "private_channel") . " with the key " . Config::getRequiredVal("chat", "private_channel_key")
 		));
